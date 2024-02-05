501361e0cdb42c7414f4f5bb7b328c540528b37d
hyungeerhee <hyungeerhee@gmail.com>
2024-02-05 13:29:58 +0900

    [이현기] -test 프로젝트 생성

---
+++ src/main/java/com/hyungeerhee/spring/test/lifecycle/Post.java
@@ -0,0 +1,43 @@
+package com.hyungeerhee.spring.test.lifecycle;
+
+public class Post {
+	
+	// 제목, 사용자, 내용
+	private String title;
+	private String user;
+	private String content;
+	
+	public Post(String title, String user, String content) {
+		
+		this.title = title;
+		this.user = user;
+		this.content = content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/hyungeerhee/spring/test/lifecycle/Test01Controller.java
@@ -0,0 +1,32 @@
+package com.hyungeerhee.spring.test.lifecycle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/lifecycle/test01")
+public class Test01Controller {
+	
+	@ResponseBody
+	@RequestMapping("/1")
+	public String welcome() {
+		return "<h2>테스트 프로젝트</h2><h4>해당 프로젝트를 통해서 문제를 풀어 봅시다</h4>";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/2")
+	public Map<String, Integer> scoreData() {
+		// 국어 : 90 수학 : 95
+		
+		Map<String, Integer> scoreMap = new HashMap<>();
+		scoreMap.put("국어", 90);
+		scoreMap.put("수학", 95);
+		scoreMap.put("영어", 100);
+		
+		return scoreMap;
+	}
+}
